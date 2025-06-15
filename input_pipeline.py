d9abae318f5c11a8444ac3b9c2ea52ef845b3866
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-06-16 00:39:32 +0100

    mnt: avoid hard coding vocab

--- input_pipeline.py
+++ input_pipeline.py
@@ -2,22 +2,6 @@ import datasets
 import jax.numpy as jnp
 
 
-# fmt: off
-VOCAB: list[str] = ["\n", " ", "!", "$", "&", "'", ",", "-", ".", "3", ":", ";", "?", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]  # noqa: E501
-# fmt: on
-
-stoi = {ch: i for i, ch in enumerate(VOCAB)}
-itos = {i: ch for i, ch in enumerate(VOCAB)}
-
-
-def encode(sentence: str) -> list[int]:
-    return [stoi[c] for c in sentence]
-
-
-def decode(latents: list[int]) -> str:
-    return "".join([itos[i] for i in latents])
-
-
 def get_datasets() -> tuple[jnp.ndarray, jnp.ndarray, int]:
     ds = datasets.load_dataset(
         "karpathy/tiny_shakespeare",
@@ -25,6 +9,19 @@ def get_datasets() -> tuple[jnp.ndarray, jnp.ndarray, int]:
         trust_remote_code=True,
     )
     train_ds, test_ds, val_ds = ds
+
+    train_text = train_ds[0]["text"]
+    vocab = sorted(set(train_text))
+
+    stoi = {ch: i for i, ch in enumerate(vocab)}
+    itos = {i: ch for i, ch in enumerate(vocab)}
+
+    def encode(sentence: str) -> list[int]:
+        return [stoi[c] for c in sentence]
+
+    def decode(latents: list[int]) -> str:
+        return "".join([itos[i] for i in latents])
+
     train_ds = datasets.concatenate_datasets([train_ds, test_ds])
 
     def encode_fn(example):
@@ -39,6 +36,6 @@ def get_datasets() -> tuple[jnp.ndarray, jnp.ndarray, int]:
     train_data = train_ds["text"][0]
     val_data = val_ds["text"][0]
 
-    vocab_size = len(VOCAB)
+    vocab_size = len(vocab)
 
     return train_data, val_data, vocab_size
